c6559548d87d5b412655469c0533fd3bec2f64c7
tomo <tomo314159@akane.waseda.jp>
2020-09-08 16:34:37 +0900

    fix: update test code
    actionの定義の仕方の変更

--- test_gym.py
+++ test_gym.py
@@ -1,6 +1,7 @@
 import gym
 # import gym_sumo
 import random
+import numpy as np
 
 # default
 # args = {
@@ -11,21 +12,36 @@ import random
 #     'mode': 'gui' (or 'cui'),
 #     'simlation_step': 100
 # }
+carnum = 100
 args = {
-    'step_length': 1,
-    'mode': 'gui'
+    'step_length': 0.01,
+    'mode': 'gui',
+    'carnum': carnum
 }
+
+# env = gym.make('gym_sumo:sumo-extrahard-v0', **args)
 env = gym.make('gym_sumo:sumo-v0', **args)
+direction = [0, 6]
+print("observation space num: ", env.observation_space.shape[0])
+print("action space num: ", env.action_space[0][0].n, env.action_space[0][1].shape[0])
 
-for i in range(10):
-    action = []
-    direction = [0, 6]
-    for i in range(100):
-        tmp = random.uniform(-1.0, 1.0)
-        tmp2 = random.randint(0, 1)
-        tmp2 = direction[tmp2]
-        action.append((tmp2, tmp))
-
-    env.step(action)
-    env.render()
+pobs = env.reset()
+done = False
+while not done:
+    act = []
+    for i in range(carnum):
+        tmp = np.array([random.uniform(0.0, 1.0)], dtype=np.float32)
+        tmp2 = 0
+        act.append((tmp2, tmp))
+    obs, reward, done, _ = env.step(act)
+    print(pobs, act, reward, obs, done)
+    pobs = obs
 env.close()
+
+# act = []
+# tmp = np.array([random.uniform(0.0, 1.0)], dtype=np.float32)
+# tmp2 = random.randint(0, 1)
+# tmp2 = direction[tmp2]
+# act.append((tmp2, tmp))
+
+# print(act, tmp, tmp2)
