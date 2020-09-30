aa7d6814dbc5e92a290348d683f4e7b1e86f296e
tomo <tomo314159@akane.waseda.jp>
2020-09-30 10:22:57 +0900

    fix: update gitignore, install file
    - .gitignore
      resultsフォルダの除外
    - install, install_gpu
      pytorchのバージョン指定を 1.6.0に固定するように変更

--- gym-sumo/install_gpu.sh
+++ gym-sumo/install_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # install torch, torch-geometric
-conda install pytorch torchvision cudatoolkit=10.2 -c pytorch
+conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.2 -c pytorch
 pip install torch-scatter==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 pip install torch-sparse==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
 pip install torch-cluster==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
