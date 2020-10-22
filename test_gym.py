f719f445c84ef46908f91657322c762d7a3c0629
tomo <tomo314159@akane.waseda.jp>
2020-10-22 18:04:54 +0900

    fix: update test code
    - test_gym
      sumo-simple-envでのコード

--- test_gym.py
+++ test_gym.py
@@ -1,7 +1,8 @@
 import gym
 import gym_sumo
-from gym_sumo.envs.sumo_light_env import RIGHT, LEFT
-import traci
+# from gym_sumo.envs.sumo_light_env import RIGHT, LEFT
+# from gym_sumo.envs.sumo_simple_env import SumoSimpleEnv
+# import traci
 
 # default
 # args = {
@@ -14,14 +15,17 @@ import traci
 # }
 carnum = 100
 args = {
-    'step_length': 0.01,
-    # 'mode': 'cui',
+    'step_length': 1,
+    # 'mode': 'cui'
     # 'carnum': carnum
 }
 
 # env = gym.make('gym_sumo:sumo-extrahard-v0', **args)
 # env = gym.make('gym_sumo:sumo-v0', **args)
-env = gym.make('sumo-light-v0', **args)
+# env = gym.make('sumo-v0', **args)
+# env = gym.make('sumo-light-v0', **args)
+env = gym.make('sumo-simple-v0', **args)
+# env = SumoSimpleEnv(**args)
 # err_msg = "%r (%s) invalid" % (env.observation, type(env.observation))
 # assert env.observation_space.contains(env.observation), err_msg
 # print(env.action_space.n)
@@ -30,12 +34,12 @@ vehID = 'veh0'
 for i in range(1000):
     env.render()
     action = env.action_space.sample()
-    action = 0
-    signal = traci.vehicle.getSignals(vehID)
-    if signal == 0:
-        action = RIGHT
-    elif signal == 1:
-        action = LEFT
+    # action = [0.0, 0.5, 0.2]
+    # signal = traci.vehicle.getSignals(vehID)
+    # if signal == 0:
+    #     action = RIGHT
+    # elif signal == 1:
+    #     action = LEFT
     observation, reward, done, _ = env.step(action)
     print(i, reward, done)
 
