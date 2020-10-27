3b1c4bd6e28e29d38b94b76a702bdf62f18d99ba
tomo <tomo314159@akane.waseda.jp>
2020-10-28 02:36:47 +0900

    feat: increased in speed, inherit sumo_base_env
    - sumo_simple_env
      - reset関数
        毎回起動するのをやめた(resetはなんか起動の代わりだと思っていた。)
      - 構造変更
        コードが長くなり、煩雑になってきたので、sumo_base_envを継承すること
        でコード量を減らした。

--- gym-sumo/gym_sumo/envs/sumo_simple_env.py
+++ gym-sumo/gym_sumo/envs/sumo_simple_env.py
@@ -1,26 +1,18 @@
-import gym
-import tempfile
+from IPython import embed
 # from gym import error, spaces, utils
 from gym import spaces, error
-from gym.utils import seeding
-from PIL import Image
-
-import os
-import sys
 
 try:
     import traci
     from traci import constants as tc
-    from traci.exceptions import TraCIException
 except ImportError as e:
     raise error.DependencyNotInstalled(
         "{}. (HINT: you can install sumo or set the path for sumo library by reading README.md)".format(e))
 
 import numpy as np
 
-from gym_sumo.envs._graph import Graph
-from gym_sumo.envs._util import randomTuple, vector_decomposition, get_degree
-from gym_sumo.envs._util import in_many_shape
+from .sumo_base_env import SumoBaseEnv as BaseEnv
+from gym_sumo.envs._util import vector_decomposition, get_degree, in_many_shape
 from gym_sumo.envs._util import get_rectangle_positions, in_rect, get_base_angle
 
 # action
@@ -33,9 +25,6 @@ PARRIGHT = 5
 STOP = 6
 
 STANDARD_SPEED = 100.0 / 9.0
-
-# standard length for adding car
-SPOS = 10.5
 
 # visible range of the car (circle with that radius to that range) [m]
 VISIBLE_RANGE = 10
@@ -69,120 +58,57 @@ VEH_SIGNALS = {
 }
 
 
-class SumoSimpleEnv(gym.Env):
+class SumoSimpleEnv(BaseEnv):
 
-    def __init__(self, isgraph=True, area='nishiwaseda', carnum=100, mode='cui',
-                 step_length=0.01, simulation_end=100, seed=None):
-        sumoConfig = 'sumo_configs/' + area
-        sumoMap = os.path.join(os.path.dirname(__file__), sumoConfig)
-        self.__netpath = os.path.join(sumoMap, 'osm.net.xml')
-
-        self.metadata = {'render.modes': ['human', 'rgb_array']}
-        self.np_img = None
-
-        self.__sumocfg = os.path.join(sumoMap, 'osm.sumocfg')
-        self.__carnum = carnum
-        self.__mode = mode
-        self.__vehIDList = {}
-        self.__removeIDList = []
-        self.__stepLength = step_length
-        self.__simulation_end = simulation_end
-        self.__isgraph = isgraph
-        self.__seed = self.seed(seed)
-        self.__graph = Graph(self.__netpath)
-        self.routeEdge = []
-        self._init_simulator(mode, step_length=step_length)
-        self._add_car(carnum)
-        # self.observation = self.reset()
-        self.vehID = list(self.__vehIDList)[0]
+    def __init__(self, isgraph=True, area=0, carnum=100, mode='gui', step_length=0.01,
+                 simulation_end=100, seed=None):
+        super().__init__(isgraph, area, carnum, mode, step_length, simulation_end, seed)
+        self.vehID = list(self._vehID_list)[0]
         # steer, accel, brake
         low = np.array([-np.inf, -1.0, 0.0])
         high = np.array([np.inf, 1.0, 1.0])
         self.action_space = spaces.Box(low=low, high=high, dtype=np.float32)
-        # self.observation_space = spaces.Box(
-        #     low=-np.inf, high=np.inf, dtype=np.float,
-        #     shape=(carnum, 2, 3 + (VISIBLE_NUM * 2)))
         self.observation_space = spaces.Box(
             low=-np.inf, high=np.inf, dtype=np.float, shape=((3 + (VISIBLE_NUM * 3)), ))
-        # self.reward_range = (-1, 1)
-        # super(SumoLightEnv, self).__init__()
-
-    def _isDone(self, vehID):
-        removeList = self.__removeIDList
-        return vehID in removeList
-
-    def screenshot_and_simulation_step(self):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            viewID = traci.gui.DEFAULT_VIEW
-            img_path = tmpdir + '/screenshot.png'
-            traci.gui.screenshot(viewID, img_path)
-            traci.simulationStep()
-            with open(img_path, 'rb') as f:
-                img = Image.open(f).convert('RGB')
-                self.np_img = np.array(img)
 
     def step(self, action):
         # determine next step action
-        self._update_add_car()
-        vehID = list(self.__vehIDList)[0]
+        vehID = list(self._vehID_list)[0]
         reward_state = (self._take_action(vehID, action))
-        if self.__mode == 'gui':
+        if self._mode == 'gui':
             self.screenshot_and_simulation_step()
         else:
             traci.simulationStep()
-        self.observation = self._observation(vehID)
+        observation = self._observation(vehID)
         reward = self.reward(vehID, reward_state)
-        isDone = self._isDone(vehID)
-        return self.observation, reward, isDone, {}
+        done = self._is_done(vehID)
+        return observation, reward, done, {}
 
     def reset(self):
-        # mode = self.__mode
-        # reset
-        # self.close()
-        # self.routeEdge = []
-        # traci start & init simulate
-        # self._init_simulator(mode=mode, step_length=self.__stepLength)
-        # self.__vehIDList.clear()
-        self.__removeIDList.clear()
-        # self._add_car(self.__carnum)
-        self._remove_car_all()
+        cur_time = traci.simulation.getTime()
+        self._reset_simulate_time(cur_time)
         self._update_add_car()
-        vehID = list(self.__vehIDList)[0]
-        if self.__mode == 'gui':
+        self._removed_vehID_list.clear()
+        vehID = list(self._vehID_list)[0]
+        if self._mode == 'gui':
             viewID = traci.gui.DEFAULT_VIEW
             traci.gui.trackVehicle(viewID, vehID)
-            zoom = traci.gui.getZoom()
-            traci.gui.setZoom(viewID, zoom * 50)
-        self.observation = self._observation(vehID)
-        if self.__mode == 'gui':
+            # zoom = traci.gui.getZoom()
+            traci.gui.setZoom(viewID, 5000)
+        observation = self._observation(vehID)
+        if self._mode == 'gui':
             self.screenshot_and_simulation_step()
         else:
             traci.simulationStep()
-        return self.observation
-
-    def render(self, mode='human'):
-        if self.__mode == 'gui':
-            if mode == 'rgb_array':
-                return self.np_img
-
-    def close(self):
-        try:
-            traci.close()
-            sys.stdout.flush()
-        except traci.exceptions.FatalTraCIError as ci:
-            print(ci)
-
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+        return observation
 
     def reward(self, vehID, reward_state):
         v_list = traci.vehicle.getIDList()
-        collisionList = traci.simulation.getCollidingVehiclesIDList()
+        collision_list = traci.simulation.getCollidingVehiclesIDList()
         reward = 0
         if vehID in v_list:
-            if vehID in collisionList:
-                self.__removeIDList.append(vehID)
+            if vehID in collision_list:
+                self._removed_vehID_list.append(vehID)
                 reward -= 1
             else:
                 if reward_state[0]:
@@ -199,13 +125,14 @@ class SumoSimpleEnv(gym.Env):
         return reward
 
     def _observation(self, vehID):
-        pos = traci.vehicle.getPosition(vehID)
+        pos = list(traci.vehicle.getPosition(vehID))
         if pos[0] == tc.INVALID_DOUBLE_VALUE or pos[1] == tc.INVALID_DOUBLE_VALUE:
             pos[0], pos[1] = -np.inf, -np.inf
-        posList = [pos]
-        neighborList = self._get_neighbor_list(posList, VISIBLE_RANGE)
+        pos_list = [pos]
+        neighbor_list = self._sumo_util._get_neighbor_list(
+            pos_list, VISIBLE_RANGE)
         level = 3 + (VISIBLE_NUM * 3)
-        for neighbor in neighborList:
+        for neighbor in neighbor_list:
             v_obs = [0.0] * level
             num = VISIBLE_NUM * 3
             speed = traci.vehicle.getSpeed(vehID)
@@ -223,90 +150,14 @@ class SumoSimpleEnv(gym.Env):
                     r_x, r_y = 0.0, 0.0
                     category = float(VEHICLE_CATEGORY[NONE_VEH_CATEGORY])
                 v_obs[j + 3], v_obs[j + 4], v_obs[j + 5] = r_x, r_y, category
-        self.observation = np.array(v_obs, dtype=np.float)
-        return self.observation
-
-    def _update_add_car(self):
-        v_list = traci.vehicle.getIDList()
-        for i, vehID in enumerate(self.__vehIDList):
-            if vehID not in self.__removeIDList and vehID not in v_list:
-                routeID = self.__vehIDList[vehID]
-                route = traci.route.getEdges(routeID)
-                laneID = route[0] + '_0'
-                laneVehIDList = traci.lane.getLastStepVehicleIDs(laneID)
-                minPos = traci.lane.getLength(laneID)
-                for vehID in laneVehIDList:
-                    lanePos = traci.vehicle.getLanePosition(vehID)
-                    minPos = min(lanePos, minPos)
-                if minPos >= SPOS:
-                    self._insert_car(vehID, routeID, route[0])
-
-    def _init_simulator(self, mode='gui', routing_alg='dijkstra', step_length=0.01):
-        sumocfg = self.__sumocfg
-        self.__stepLength = step_length
-        sumoCommand = mode
-        if mode == 'gui':
-            sumoCommand = 'sumo-gui'
-        elif mode == 'cui':
-            sumoCommand = 'sumo'
-        else:
-            raise AttributeError("not supported mode!!")
-        sumoCmd = [sumoCommand, '-c', sumocfg, '--routing-algorithm', routing_alg,
-                   '--step-length', str(step_length), '--collision.action', 'remove',
-                   '--collision.check-junctions', 'true', '--tls.all-off', 'true',
-                   '--no-warnings', 'true',
-                   '--no-step-log', 'true', '--duration-log.disable', 'true']
-        traci.start(sumoCmd, numRetries=100)
-
-    def _add_car(self, carnum):
-        for i in range(carnum):
-            vehID = 'veh{}'.format(i)
-            routeID = 'route{}'.format(i)
-            fromEdgeID, toEdgeID = self._generate_route(routeID)
-            veh_element = {'route': routeID,
-                           'start': fromEdgeID, 'goal': toEdgeID}
-            self.__vehIDList[vehID] = veh_element
-            start = self.__graph.getEdgeIndex(fromEdgeID)
-            if start not in self.routeEdge:
-                self.routeEdge.append(start)
-                self._insert_car(vehID, routeID, fromEdgeID)
-
-    def _insert_car(self, vehID, routeID, startEdgeID=None):
-        if startEdgeID is None:
-            startEdgeID = traci.route.getEdges(routeID)[0]
-        # register car
-        traci.vehicle.add(vehID, routeID)
-        # set speed mode
-        traci.vehicle.setSpeedMode(vehID, 0)
-        laneID = startEdgeID + '_0'
-        length = min(traci.vehicle.getLength(vehID),
-                     traci.lane.getLength(laneID))
-        # insert car instantly
-        traci.vehicle.moveTo(vehID, laneID, length)
-        traci.vehicle.setSpeed(vehID, 0.0)
+        observation = np.array(v_obs, dtype=np.float)
+        return observation
 
     def _remove_car_all(self):
-        v_list = traci.vehicle.getIDList()
+        # v_list = traci.vehicle.getIDList()
+        v_list = list(self._vehID_list)
         for vehID in v_list:
             traci.vehicle.remove(vehID)
-
-    def _generate_route(self, routeID):
-        num_edge = self.__graph.getNum('edge_normal') - 1
-        fromEdgeID = None
-        toEdgeID = None
-        for i in range(10):
-            # print(i, "test", routeID, "route")
-            edges = randomTuple(0, num_edge, 2, self.routeEdge, self.np_random)
-            fromEdgeID = self.__graph.getEdgeID(edges[0])
-            toEdgeID = self.__graph.getEdgeID(edges[1])
-            try:
-                route = traci.simulation.findRoute(fromEdgeID, toEdgeID)
-                if len(route.edges) > 0:
-                    traci.route.add(routeID, route.edges)
-                    break
-            except TraCIException as tr:
-                print(tr)
-        return fromEdgeID, toEdgeID
 
     def _take_action(self, vehID, action):
         steer, accel, brake = action
@@ -352,41 +203,3 @@ class SumoSimpleEnv(gym.Env):
                            or in_many_shape(lane_pos, rear_pos))
         traci.vehicle.moveToXY(vehID, edgeID, lane, next_x, next_y, steer, 2)
         return in_road, match_degree
-
-    def _is_exist(self, pos, r, target):
-        posx, posy = pos
-
-        def distance(targetpos):
-            x, y = targetpos
-            return np.sqrt((posx - x)**2 + (posy - y)**2)
-
-        dis = distance(target)
-        relative_pos = [x - y for (x, y) in zip(target, pos)]
-        return dis <= r, relative_pos
-
-    def _get_neighbor(self, posList, central_index, visible_range):
-        neighborList = []
-        centralpos = posList[central_index]
-        for i, pos in enumerate(posList):
-            if i == central_index:
-                continue
-            x, y = pos
-            if x == -np.inf or y == -np.inf:
-                isexist, dis = False, np.inf
-            else:
-                isexist, dis = self._is_exist(centralpos, visible_range, pos)
-            if isexist:
-                tmp = [i, dis]
-                neighborList.append(tmp)
-        return neighborList
-
-    def _get_neighbor_list(self, posList, visible_range):
-        neighborList = []
-        for index, pos in enumerate(posList):
-            x, y = pos
-            if x != -np.inf and y != -np.inf:
-                tmp = self._get_neighbor(posList, index, visible_range)
-            else:
-                tmp = []
-            neighborList.append(tmp)
-        return neighborList
