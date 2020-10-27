729bed187eb2b59fbbe55553e9213aecefddb0da
tomo <tomo314159@akane.waseda.jp>
2020-10-28 02:45:59 +0900

    feat: add requirements packages
    使用しているパッケージが増えたので追加

--- gym-sumo/setup.py
+++ gym-sumo/setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 setup(name='gym_sumo',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'matplotlib', 'networkx']  # And any other dependencies foo needs
-)
+      # And any other dependencies foo needs
+      install_requires=['gym', 'numpy', 'matplotlib', 'pillow'
+                        'networkx', 'shapely', 'ipython']
+      )
