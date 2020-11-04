f1b6df8c9d080b33205cfa862343d1320b4045d7
tomo <tomo314159@akane.waseda.jp>
2020-11-04 16:45:21 +0900

    fix: add process for multiple env, code format
    - sumo_base_env
      - init_simulator
        1度に2つ以上の環境を立ち上げられるような処理を追加
      - generate_route
        ルートの作成の処理順の変更
        -> routeを探す関数を分けて、探す回数の制限をできるようにした。
      - 雑務
        コードのフォーマット(変数名をpythonの命名規則に沿うように)
        init_simulator, generate_routeの変更に伴う変更

--- gym-sumo/gym_sumo/envs/sumo_base_env.py
+++ gym-sumo/gym_sumo/envs/sumo_base_env.py
@@ -7,7 +7,6 @@ import sys
 
 try:
     import traci
-    from traci.exceptions import TraCIException
 except ImportError as e:
     raise error.DependencyNotInstalled(
         "{}. (HINT: you can install sumo or set the path for sumo library by reading README.md)".format(e))
@@ -18,7 +17,7 @@ import tempfile
 from IPython import embed  # for debug
 
 from ._graph import Graph
-from ._util import randomTuple, get_base_vector
+from ._util import random_tuple, get_base_vector
 from .sumo_util import SumoUtil
 
 AREA = ['nishiwaseda', 'waseda_university']
@@ -61,7 +60,7 @@ class SumoBaseEnv(gym.Env):
     sumo_label = []
 
     def __init__(self, isgraph=True, area=0, carnum=100, mode='gui', step_length=0.01,
-                 simulation_end=100, seed=None):
+                 simulation_end=100, seed=None, label='default'):
         super().__init__()
         sumo_config = 'sumo_configs/' + AREA[area]
         sumo_map = os.path.join(os.path.dirname(__file__), sumo_config)
@@ -73,6 +72,7 @@ class SumoBaseEnv(gym.Env):
         self._sumocfg = os.path.join(sumo_map, 'osm.sumocfg')
         self._carnum = carnum
         self._mode = mode
+        self.label = label
         self._vehID_list = {}
         self._goal = {}
         self._removed_vehID_list = []
@@ -94,7 +94,7 @@ class SumoBaseEnv(gym.Env):
 
     def close(self):
         try:
-            traci.close()
+            self.traci_connect.close()
             sys.stdout.flush()
         except traci.exceptions.FatalTraCIError as ci:
             print(ci)
@@ -105,8 +105,8 @@ class SumoBaseEnv(gym.Env):
 
     def _is_done(self, vehID):
         removed_list = self._removed_vehID_list
-        v_list = traci.vehicle.getIDList()
-        cur_time = traci.simulation.getTime() - self._cur_simulation_start
+        v_list = self.traci_connect.vehicle.getIDList()
+        cur_time = self.traci_connect.simulation.getTime() - self._cur_simulation_start
         return (vehID in removed_list
                 or vehID not in v_list
                 or cur_time >= self._simulation_end)
@@ -118,7 +118,7 @@ class SumoBaseEnv(gym.Env):
         self._vehID_list[vehID]['goal'] = goal_edgeID
         goal_laenID = goal_edgeID + "_0"
         goal_element = {}
-        lane_pos = traci.lane.getShape(goal_laenID)
+        lane_pos = self.traci_connect.lane.getShape(goal_laenID)
         goal_element['pos'] = list(lane_pos[1])
         goal_element['direct'] = get_base_vector(lane_pos[0], lane_pos[1])
         self._goal[vehID] = goal_element
@@ -136,28 +136,35 @@ class SumoBaseEnv(gym.Env):
                     '--step-length', str(step_length), '--collision.action', 'remove',
                     '--collision.check-junctions', 'true', '--tls.all-off', 'true',
                     ]
-        traci.start(sumo_cmd, numRetries=100)
+        label = self.label
+        traci_label = SumoBaseEnv.sumo_label
+        if label in traci_label:
+            index = len(traci_label)
+            label = label + '{}'.format(index)
+        self.label = label
+        traci.start(sumo_cmd, numRetries=100, label=label)
+        self.traci_connect = traci.getConnection(label)
 
     def _add_car(self, carnum):
         for i in range(carnum):
             vehID = 'veh{}'.format(i)
-            routeID = 'route{}'.format(i)
-            from_edgeID, to_edgeID = self._generate_route(routeID)
+            routeID, start_end_edge = self._generate_route(i)
+            from_edgeID, to_edgeID = start_end_edge
             veh_element = {'route': routeID,
                            'start': from_edgeID, 'goal': to_edgeID}
             self._vehID_list[vehID] = veh_element
             self._reset_goal_element(vehID, to_edgeID)
             self._start_edge_list.append(self._graph.getEdgeIndex(from_edgeID))
-            traci.vehicle.add(vehID, routeID)
-            traci.vehicle.setSpeedMode(vehID, 0)
-            traci.vehicle.setSpeed(vehID, 0.0)
+            self.traci_connect.vehicle.add(vehID, routeID)
+            self.traci_connect.vehicle.setSpeedMode(vehID, 0)
+            self.traci_connect.vehicle.setSpeed(vehID, 0.0)
+            self.traci_connect.simulationStep()
 
-        traci.simulationStep()
-        self._reset_simulate_time(traci.simulation.getTime())
+        self._reset_simulate_time(self.traci_connect.simulation.getTime())
 
     def _reposition_car(self):
-        traci.simulationStep()
-        v_list = traci.vehicle.getIDList()
+        self.traci_connect.simulationStep()
+        v_list = self.traci_connect.vehicle.getIDList()
         for i, vehID in enumerate(self._vehID_list):
             if vehID in self._removed_vehID_list and vehID not in v_list:
                 routeID = self._vehID_list[vehID]['route']
@@ -165,54 +172,73 @@ class SumoBaseEnv(gym.Env):
                 goal_edgeID = self._sumo_util.get_target(routeID=routeID)
                 self._reset_goal_element(vehID, goal_edgeID)
                 laneID = edgeID + '_0'
-                lane_vehID_list = traci.lane.getLastStepVehicleIDs(laneID)
-                min_pos = max(SPOS, traci.lane.getLength(laneID))
+                lane_vehID_list = self.traci_connect.lane.getLastStepVehicleIDs(
+                    laneID)
+                min_pos = max(SPOS, self.traci_connect.lane.getLength(laneID))
                 for vehID in lane_vehID_list:
-                    lane_pos = traci.vehicle.getLanePosition(vehID)
+                    lane_pos = self.traci_connect.vehicle.getLanePosition(
+                        vehID)
                     min_pos = min(lane_pos, min_pos)
                 if min_pos >= SPOS:
-                    traci.vehicle.add(vehID, routeID)
-                    traci.vehicle.setSpeedMode(vehID, 0)
-                    traci.vehicle.setSpeed(vehID, 0.0)
-        traci.simulationStep()
-        self._reset_simulate_time(traci.simulation.getTime())
+                    self.traci_connect.vehicle.add(vehID, routeID)
+                    self.traci_connect.vehicle.setSpeedMode(vehID, 0)
+                    self.traci_connect.vehicle.setSpeed(vehID, 0.0)
+            self.traci_connect.simulationStep()
 
-    def _generate_route(self, routeID):
+        self._reset_simulate_time(self.traci_connect.simulation.getTime())
+
+    def _generate_route(self, index):
+        exclude = []
+        routeID = 'route{}'.format(index)
+        is_infinite, is_find = (index == 0), False
+        find_num, max_find_num = 0, 10
+        while not is_find:
+            if not is_infinite and find_num >= max_find_num:
+                other_index = self.np_random.randint(0, index)
+                routeID = 'route{}'.format(other_index)
+                route = self.traci_connect.route.getEdges(routeID)
+                start_end_edge = [route[0], route[len(route) - 1]]
+                break
+            is_find, exclude_edge, start_end_edge = self._find_route(
+                routeID, exclude)
+            exclude.append(exclude_edge)
+            find_num += 1
+
+        return routeID, start_end_edge
+
+    def _find_route(self, routeID, exclude=[]):
         num_edge = self._graph.getNum('edge_normal') - 1
-        from_edgeID = None
-        to_edgeID = None
-        for i in range(10):
-            edges = randomTuple(
-                0, num_edge, 2, self._start_edge_list, self.np_random)
-            from_edgeID = self._graph.getEdgeID(edges[0])
-            to_edgeID = self._graph.getEdgeID(edges[1])
-            try:
-                route = traci.simulation.findRoute(from_edgeID, to_edgeID)
-                if len(route.edges) > 0:
-                    traci.route.add(routeID, route.edges)
-                    break
-            except TraCIException as tr:
-                print(tr)
-        return from_edgeID, to_edgeID
+        is_find = False
+        edges = random_tuple(
+            0, num_edge, 2, self._start_edge_list, exclude, self.np_random)
+        from_edgeID = self._graph.getEdgeID(edges[0])
+        to_edgeID = self._graph.getEdgeID(edges[1])
+        if edges in exclude:
+            return is_find, edges, [from_edgeID, to_edgeID]
+        route = self.traci_connect.simulation.findRoute(from_edgeID, to_edgeID)
+        if len(route.edges) > 0:
+            self.traci_connect.route.add(routeID, route.edges)
+            is_find = True
+        return is_find, edges, [from_edgeID, to_edgeID]
 
     def _insert_car(self, vehID, routeID, startEdgeID=None):
         if startEdgeID is None:
-            startEdgeID = traci.route.getEdges(routeID)[0]
+            startEdgeID = self.traci_connect.route.getEdges(routeID)[0]
         # set speed mode
-        traci.vehicle.setSpeedMode(vehID, 0)
+        self.traci_connect.vehicle.setSpeedMode(vehID, 0)
         laneID = startEdgeID + '_0'
-        length = min(traci.vehicle.getLength(vehID),
-                     traci.lane.getLength(laneID))
+        length = min(self.traci_connect.vehicle.getLength(vehID),
+                     self.traci_connect.lane.getLength(laneID))
         # insert car instantly
-        traci.vehicle.moveTo(vehID, laneID, length)
-        traci.vehicle.setSpeed(vehID, 0.0)
+        self.traci_connect.vehicle.moveTo(vehID, laneID, length)
+        self.traci_connect.vehicle.setSpeed(vehID, 0.0)
 
     def screenshot_and_simulation_step(self):
         with tempfile.TemporaryDirectory() as tmpdir:
-            viewID = traci.gui.DEFAULT_VIEW
+            viewID = self.traci_connect.gui.DEFAULT_VIEW
             img_path = tmpdir + '/screenshot.png'
-            traci.gui.screenshot(viewID, img_path)
-            traci.simulationStep()
+            self.traci_connect.gui.screenshot(viewID, img_path)
+            self.traci_connect.simulationStep()
             with open(img_path, 'rb') as f:
                 img = Image.open(f).convert('RGB')
                 self.np_img = np.array(img)
