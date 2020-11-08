39f6269b891c79f02cefd5d17018303e8408896f
tomo <tomo314159@akane.waseda.jp>
2020-11-08 13:17:53 +0900

    fix: update install script
    - requirements
      tensorboardの追加
      pythonのバージョンの追加
    - setup
      スペルミスの訂正
      tensorboardの追加
    - .gitignore
      log, errorファイルの追加

--- gym-sumo/setup.py
+++ gym-sumo/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='gym_sumo',
       version='0.0.1',
       # And any other dependencies foo needs
-      install_requires=['gym', 'numpy', 'matplotlib', 'pillow'
-                        'networkx', 'shapely', 'ipython']
+      install_requires=['gym', 'numpy', 'matplotlib', 'pillow',
+                        'networkx', 'shapely', 'ipython', 'tensorboard']
       )
