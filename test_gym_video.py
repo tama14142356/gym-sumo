5c140c54273802b88d703af237a3c04aaed5f1b2
tomo <tomo314159@akane.waseda.jp>
2020-11-15 20:35:08 +0900

    fix: upate test code for sumo-light
    sumo-light用に変更し、実行するのも死んだら終わりではなく、1000回必ず
    動くようにした。

--- test_gym_video.py
+++ test_gym_video.py
@@ -1,19 +1,22 @@
 import gym
 import gym_sumo
 
-args = {
+kwargs = {
     "step_length": 1,
     "mode": "gui",
+    "carnum": 1,
 }
 
-env = gym.make("sumo-simple-v0", **args)
+env = gym.make("sumo-light-v0", **kwargs)
 env = gym.wrappers.Monitor(env, "./video", force=True)
 
 done = False
 observation = env.reset()
-while not done:
+for i in range(1000):
     # env.render()
     action = env.action_space.sample()
     observation, reward, done, _ = env.step(action)
+    if done:
+        observation = env.reset()
 
 env.close()
