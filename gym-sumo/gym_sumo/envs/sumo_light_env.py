97239fe559d83af8d686da0c1a72140c5807ae33
tomo <tomo314159@akane.waseda.jp>
2020-12-01 06:27:29 +0900

    fix: correct reward
    ゴールまでの距離をそのまま報酬として与えてしまっていたので、それの正負を
    反転させた

--- gym-sumo/gym_sumo/envs/sumo_light_env.py
+++ gym-sumo/gym_sumo/envs/sumo_light_env.py
@@ -97,10 +97,10 @@ class SumoLightEnv(BaseEnv):
                 cur_driving_len = self.traci_connect.vehicle.getDistance(vehID)
                 routeID = self._vehID_list[vehID]["route"]
                 total_length = self._route_list[routeID]["length"]
-                to_goal_length = total_length - cur_driving_len
+                to_goal_length = cur_driving_len - total_length
                 info["driving_len"] = cur_driving_len
                 reward += info["speed"]
-                reward += to_goal_length if total_length > 0 else 0.0
+                reward += to_goal_length if total_length > 0 else -1000.0
 
         done = self._is_done(vehID)
         observation = self._observation(vehID, done, info["is_arrived"])
