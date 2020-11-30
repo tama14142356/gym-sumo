37178bd8dc300f99fa179a156b81a21211cbc1b0
tomoya <tomo314159@akane.waseda.jp>
2020-12-01 06:00:34 +0900

    feat: add font install commend
    pillowで使うフォントをインストールするコマンドを追加

--- gym-sumo/install_gpu.sh
+++ gym-sumo/install_gpu.sh
@@ -10,3 +10,14 @@ pip install torch-geometric
 # other requirement packages
 rm -r *.egg-info/
 pip install -e .
+
+# install fonts
+mkdir -p $HOME/local_fonts/fonts
+pushd $HOME/local_fonts/
+git clone git@github.com:python-pillow/Pillow.git
+cp Pillow/Tests/fonts/* fonts/
+rm -rf Pillow
+popd
+echo '' >> ~/.bashrc
+echo '# pillow fonts' >> ~/.bashrc
+echo 'export XDG_DATA_DIRS="$HOME/local_fonts/"' >> ~/.bashrc
