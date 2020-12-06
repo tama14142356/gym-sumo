811a1a279549c56d1412b7a52d04f89220f0479d
tomo <tomo314159@akane.waseda.jp>
2020-12-06 15:06:33 +0900

    fix: correct install command
    インストール時のコマンドが間違っていたので訂正

--- gym-sumo/install_gpu.sh
+++ gym-sumo/install_gpu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # install torch, torch-geometric
 conda install pytorch==1.6.0 torchvision==0.7.0 cudatoolkit=10.2 -c pytorch
-pip install torch-scatter==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
-pip install torch-sparse==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
-pip install torch-cluster==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
-pip install torch-spline-conv==latest+cu102 -f https://pytorch-geometric.com/whl/torch-1.6.0.html
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html
 pip install torch-geometric
 
 # other requirement packages
