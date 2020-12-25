133fccd0d96eea1129377d0baf2b773bef8269d3
tomo <tomo314159@akane.waseda.jp>
2020-12-25 14:08:16 +0900

    fix: correct route data format, way to save
    ルートのデータのフォーマットの変更
    ->情報を追加
    保存するときにスタートとゴールの距離でソートして保存

--- gym-sumo/gym_sumo/envs/route_data_save.py
+++ gym-sumo/gym_sumo/envs/route_data_save.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pathlib
 import pickle
+import pprint
 
 from gym import error
 import gym_sumo.envs.sumo_base_env as base
@@ -48,24 +49,36 @@ def format_data_num_dir(data_num):
     return "{}_data".format(data_num)
 
 
-def route_data_save(start_end_list, file_path=str(LOAD_DEFAULT_DIR)):
+def route_data_save(route_data, file_path=str(LOAD_DEFAULT_DIR)):
     tmp_path = pathlib.Path(file_path)
     route_data_file_name = OUTPUT_FILE_NAME
     route_data_text_file_name = TEXT_FILE_NAME
     output_path = tmp_path / route_data_file_name
     with open(output_path, "wb") as f:
-        pickle.dump(start_end_list, f)
+        pickle.dump(route_data, f)
     output_path = tmp_path / route_data_text_file_name
     with open(output_path, "w") as f:
-        print(start_end_list, file=f)
+        pprint.pprint(route_data, stream=f, compact=True)
 
 
-def route_data_load(file_path=str(LOAD_DEFAULT_DIR)):
+def route_data_load(file_path=str(LOAD_DEFAULT_DIR), in_length=False, is_all=False):
     tmp_path = pathlib.Path(file_path)
     route_data_file_name = OUTPUT_FILE_NAME
     output_path = tmp_path / route_data_file_name
     with open(output_path, "rb") as f:
-        start_end_list = pickle.load(f)
+        data = pickle.load(f)
+    if is_all:
+        return data
+    if in_length:
+        start_end_list = [
+            {"length": d.get("abs_length"), "start_end": [d.get("start"), d.get("end")]}
+            for d in data
+            if d.get("abs_length") and d.get("start") and d.get("end")
+        ]
+        return start_end_list
+    start_end_list = [
+        [d.get("start"), d.get("end")] for d in data if d.get("start") and d.get("end")
+    ]
     return start_end_list
 
 
@@ -130,16 +143,27 @@ def main(area, net_file_name, sumocfg_file_name, length, max_data):
             cur_length = calc_distance(start_pos, end_pos)
             if cur_length >= min_length and cur_length <= max_length:
                 route = traci_conn.simulation.findRoute(start_edgeID, target_edgeID)
-                if len(route.edges) > 0:
+                edge_num = len(route.edges)
+                if edge_num > 0:
+                    route_info = {
+                        "start": start_edgeID,
+                        "end": target_edgeID,
+                        "abs_length": cur_length,
+                        "route_length": route.length,
+                        "cost": route.cost,
+                        "travel_time": route.travelTime,
+                        "edge_list": route.edges,
+                        "edge_num": edge_num,
+                    }
                     route_data_num += 1
-                    route_data_list.append([start_edgeID, target_edgeID])
+                    route_data_list.append(route_info)
                     progress_bar.update(1)
                     if route_data_num >= max_data:
                         break
         if route_data_num >= max_data:
             break
-
-    route_data_save(route_data_list, str(route_dir_path))
+    route_data_list_sorted = sorted(route_data_list, key=lambda x: x["abs_length"])
+    route_data_save(route_data_list_sorted, str(route_dir_path))
     try:
         traci_conn.close()
     except traci_conn.exceptions.FatalTraCIError as ci:
