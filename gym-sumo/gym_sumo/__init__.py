3fe12f8acc1000b116f22297ae6f80ac9e891b67
tomo <tomo314159@akane.waseda.jp>
2020-12-27 16:47:46 +0900

    fix: update register info
    max_stepsに応じて環境の最大ステップ数も指定するように変更
    更に最大の長さも指定

--- gym-sumo/gym_sumo/__init__.py
+++ gym-sumo/gym_sumo/__init__.py
@@ -3,21 +3,25 @@ from gym.envs.registration import register
 register(
     id="sumo-light-v0",
     entry_point="gym_sumo.envs:SumoLightEnv",
+    kwargs={"simulation_end": 200, "max_length": 100.0},
     max_episode_steps=200,
 )
 register(
     id="sumo-light-v1",
     entry_point="gym_sumo.envs:SumoLightEnv",
+    kwargs={"simulation_end": 500, "max_length": 250.0},
     max_episode_steps=500,
 )
 
 register(
     id="sumo-fix-v0",
     entry_point="gym_sumo.envs:SumoFixedEnv",
+    kwargs={"simulation_end": 200, "max_length": 100.0},
     max_episode_steps=200,
 )
 register(
     id="sumo-fix-v1",
     entry_point="gym_sumo.envs:SumoFixedEnv",
+    kwargs={"simulation_end": 500, "max_length": 250.0},
     max_episode_steps=500,
 )
