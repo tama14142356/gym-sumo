d0ce0dafcd48a7e165203deaadc875668ff32e1a
tomo <tomo314159@akane.waseda.jp>
2021-01-06 14:46:38 +0900

    fix: correct install command for pillow fonts path
    環境変数`XDG_DATA_DIRS`の設定の仕方を間違えていたので、訂正
    ついでにbashファイルのフォーマットを整えた

--- gym-sumo/install.sh
+++ gym-sumo/install.sh
@@ -8,16 +8,19 @@ pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.6.0+c
 pip install torch-geometric
 
 # other requirement packages
-rm -r *.egg-info/
+rm -r ./*.egg-info/
 pip install -e .
 
 # install fonts
-mkdir -p $HOME/local_fonts/fonts
-pushd $HOME/local_fonts/
+mkdir -p "$HOME"/local_fonts/fonts
+pushd "$HOME"/local_fonts/ || exit
 git clone git@github.com:python-pillow/Pillow.git
 cp Pillow/Tests/fonts/* fonts/
 rm -rf Pillow
-popd
-echo '' >> ~/.bashrc
-echo '# pillow fonts' >> ~/.bashrc
-echo 'export XDG_DATA_DIRS="$HOME/local_fonts/"' >> ~/.bashrc
+popd || exit
+{
+        echo ''
+        echo '# pillow fonts'
+        # shellcheck disable=SC2016
+        echo 'export XDG_DATA_DIRS="$HOME/local_fonts/:$XDG_DATA_DIRS"'
+} >> ~/.bashrc
